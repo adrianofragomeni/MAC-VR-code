bb57d438ee0bfae33ad52e05fab018a16c4166d1
Adriano Fragomeni <31849297+adrianofragomeni@users.noreply.github.com>
2025-11-02 19:52:01 +0000

    Update train_multigpu_msrvtt.sh

--- src/train_multigpu_msrvtt.sh
+++ src/train_multigpu_msrvtt.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #SBATCH --job-name=training.py
-#SBATCH --partition=mlcnu
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --time=20:00:00
@@ -9,7 +8,6 @@
 #SBATCH --gpus-per-task=4
 #SBATCH --cpus-per-task=15
 #SBATCH --mem=80GB 
-#SBATCH --account=cosc028886
 
 
 module --force purge
